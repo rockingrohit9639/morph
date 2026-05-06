ddd894ca7c042ec31d4f3b23f7a73e93cb693bd7
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2026-05-06 22:11:38 +0200

    [feat] expose APIs from core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,8 @@
-export {};
+import { Morph } from "./lib/morph";
+
+export { Morph } from "./lib/morph";
+export { LocalStorageAdapter } from "./lib/storage";
+export type { MorphConfig, MorphGroupConfig, MorphItemData, MorphRankedItem, MorphStorage } from "./lib/types";
+
+/** Default singleton instance — use this for most cases. */
+export const morph = new Morph();
